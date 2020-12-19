f9f5056fb5ec7d4d4c836a019372f00193d00802
Alex <alexyao2015@gmail.com>
2020-12-19 00:14:01 -0600

    Commit initial dockerfiles

---
+++ Dockerfile.builder
@@ -0,0 +1,331 @@
+FROM debian:buster as builder
+WORKDIR /zmbuild
+
+# Skip interactive post-install scripts
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Install base toolset
+RUN apt-get update \
+    && apt-get install -y \
+        sudo \
+        git \
+        cmake \
+        build-essential \
+        wget
+
+# Required for libmp4v2-dev
+RUN echo "deb [trusted=yes] https://zmrepo.zoneminder.com/debian/release-1.34 buster/" >> /etc/apt/sources.list \
+    && wget -O - https://zmrepo.zoneminder.com/debian/archive-keyring.gpg | apt-key add -
+
+# Install Deps
+RUN apt-get install -y \
+        default-libmysqlclient-dev \
+        libavdevice-dev \
+        libavformat-dev \
+        libcurl4-gnutls-dev \
+        libdate-manip-perl \
+        libdbd-mysql-perl \
+        libdbi-perl \
+        libdistro-info-perl \
+        libgcrypt20-dev \
+        libgnutls28-dev \
+        libjpeg-dev \
+        libjwt-gnutls-dev \
+        libmp4v2-dev \
+        libpcre3-dev \
+        libpolkit-gobject-1-dev \
+        libsys-mmap-perl \
+        libv4l-dev \
+        libvlc-dev \
+        libvncserver-dev \
+        libx264-dev
+
+
+RUN git clone --recursive https://github.com/ZoneMinder/zoneminder.git . \
+    && cmake . \
+    && make \
+    && make DESTDIR="/zminstall" install
+
+FROM debian:buster
+
+# Install ZM Dependencies
+RUN apt-get update \
+    && apt-get install -y \
+        gnupg \
+        apt-transport-https \
+        wget \
+        ca-certificates \
+        apache2 \
+        apache2-bin \
+        apache2-data \
+        apache2-utils \
+        bsd-mailx \
+        cron \
+        dmsetup \
+        docutils-common \
+        docutils-doc \
+        exim4-base \
+        exim4-config \
+        exim4-daemon-light \
+        ffmpeg \
+        fonts-font-awesome \
+        fonts-lato \
+        fonts-roboto-slab \
+        galera-3 \
+        gawk \
+        javascript-common \
+        libaio1 \
+        libalgorithm-c3-perl \
+        libapache2-mod-php \
+        libapache2-mod-php7.3 \
+        libappconfig-perl \
+        libapr1 \
+        libaprutil1 \
+        libaprutil1-dbd-sqlite3 \
+        libaprutil1-ldap \
+        libarchive-zip-perl \
+        libargon2-1 \
+        libauthen-sasl-perl \
+        libavresample4 \
+        libb-hooks-endofscope-perl \
+        libb-hooks-op-check-perl \
+        libbrotli1 \
+        libcgi-fast-perl \
+        libcgi-pm-perl \
+        libclass-c3-perl \
+        libclass-c3-xs-perl \
+        libclass-data-inheritable-perl \
+        libclass-inspector-perl \
+        libclass-load-perl \
+        libclass-method-modifiers-perl \
+        libclass-mix-perl \
+        libclass-singleton-perl \
+        libclass-std-fast-perl \
+        libclass-std-perl \
+        libclass-xsaccessor-perl \
+        libconfig-inifiles-perl \
+        libconvert-binhex-perl \
+        libcpanel-json-xs-perl \
+        libcrypt-eksblowfish-perl \
+        libcrypt-rijndael-perl \
+        libcryptsetup12 \
+        libdata-dump-perl \
+        libdata-entropy-perl \
+        libdata-float-perl \
+        libdata-optlist-perl \
+        libdatetime-locale-perl \
+        libdatetime-perl \
+        libdatetime-timezone-perl \
+        libdevel-callchecker-perl \
+        libdevel-caller-perl \
+        libdevel-lexalias-perl \
+        libdevel-stacktrace-perl \
+        libdevice-serialport-perl \
+        libdevmapper1.02.1 \
+        libdynaloader-functions-perl \
+        libemail-date-format-perl \
+        libencode-locale-perl \
+        libestr0 \
+        libeval-closure-perl \
+        libexception-class-perl \
+        libfastjson4 \
+        libfcgi-perl \
+        libfile-listing-perl \
+        libfile-sharedir-perl \
+        libfile-slurp-perl \
+        libfont-afm-perl \
+        libgd3 \
+        libhtml-form-perl \
+        libhtml-format-perl \
+        libhtml-parser-perl \
+        libhtml-tagset-perl \
+        libhtml-template-perl \
+        libhtml-tree-perl \
+        libhttp-cookies-perl \
+        libhttp-daemon-perl \
+        libhttp-date-perl \
+        libhttp-lite-perl \
+        libhttp-message-perl \
+        libhttp-negotiate-perl \
+        libimage-base-bundle-perl \
+        libimage-info-perl \
+        libimagequant0 \
+        libio-html-perl \
+        libio-interface-perl \
+        libio-pty-perl \
+        libio-sessiondata-perl \
+        libio-socket-multicast-perl \
+        libio-socket-ssl-perl \
+        libio-string-perl \
+        libio-stringy-perl \
+        libip4tc0 \
+        libjs-jquery \
+        libjs-modernizr \
+        libjs-mootools \
+        libjs-sphinxdoc \
+        libjs-underscore \
+        libjson-c3 \
+        libjson-maybexs-perl \
+        libkmod2 \
+        liblcms2-2 \
+        liblockfile-bin \
+        liblockfile1 \
+        liblognorm5 \
+        liblua5.2-0 \
+        liblwp-mediatypes-perl \
+        liblwp-protocol-https-perl \
+        libmailtools-perl \
+        libmime-lite-perl \
+        libmime-tools-perl \
+        libmime-types-perl \
+        libmodule-implementation-perl \
+        libmodule-runtime-perl \
+        libmro-compat-perl \
+        libmp4v2-2 \
+        libnamespace-autoclean-perl \
+        libnamespace-clean-perl \
+        libnet-http-perl \
+        libnet-libidn-perl \
+        libnet-sftp-foreign-perl \
+        libnet-smtp-ssl-perl \
+        libnet-ssleay-perl \
+        libnss-systemd \
+        libnumber-bytes-human-perl \
+        libossp-uuid-perl \
+        libossp-uuid16 \
+        libpackage-stash-perl \
+        libpackage-stash-xs-perl \
+        libpadwalker-perl \
+        libpam-systemd \
+        libpaper-utils \
+        libpaper1 \
+        libparams-classify-perl \
+        libparams-util-perl \
+        libparams-validationcompiler-perl \
+        libphp-serialization-perl \
+        libpolkit-backend-1-0 \
+        libpopt0 \
+        libpython-stdlib \
+        libpython2-stdlib \
+        libpython2.7-minimal \
+        libpython2.7-stdlib \
+        libreadline5 \
+        libreadonly-perl \
+        libref-util-perl \
+        libref-util-xs-perl \
+        librole-tiny-perl \
+        libsigsegv2 \
+        libsoap-lite-perl \
+        libsoap-wsdl-perl \
+        libspecio-perl \
+        libsub-exporter-perl \
+        libsub-exporter-progressive-perl \
+        libsub-identify-perl \
+        libsub-install-perl \
+        libsub-name-perl \
+        libsub-quote-perl \
+        libsys-cpu-perl \
+        libsys-meminfo-perl \
+        libtask-weaken-perl \
+        libtemplate-perl \
+        libterm-readkey-perl \
+        libtimedate-perl \
+        libtry-tiny-perl \
+        liburi-encode-perl \
+        liburi-perl \
+        libvariable-magic-perl \
+        libwebpdemux2 \
+        libwww-perl \
+        libwww-robotrules-perl \
+        libxml-libxml-perl \
+        libxml-namespacesupport-perl \
+        libxml-parser-perl \
+        libxml-sax-base-perl \
+        libxml-sax-expat-perl \
+        libxml-sax-perl \
+        libxmlrpc-lite-perl \
+        libxpm4 \
+        logrotate \
+        lsof \
+        mariadb-client \
+        mariadb-client-10.3 \
+        mariadb-client-core-10.3 \
+        mariadb-server \
+        mariadb-server-10.3 \
+        mariadb-server-core-10.3 \
+        perl-openssl-defaults \
+        php-apcu \
+        php-apcu-bc \
+        php-common \
+        php-gd \
+        php-mysql \
+        php7.3-cli \
+        php7.3-common \
+        php7.3-gd \
+        php7.3-json \
+        php7.3-mysql \
+        php7.3-opcache \
+        php7.3-phpdbg \
+        php7.3-readline \
+        policykit-1 \
+        python \
+        python-alabaster \
+        python-asn1crypto \
+        python-babel \
+        python-babel-localedata \
+        python-certifi \
+        python-cffi-backend \
+        python-chardet \
+        python-cryptography \
+        python-docutils \
+        python-enum34 \
+        python-idna \
+        python-imagesize \
+        python-ipaddress \
+        python-jinja2 \
+        python-markupsafe \
+        python-minimal \
+        python-olefile \
+        python-openssl \
+        python-packaging \
+        python-pil \
+        python-pkg-resources \
+        python-pygments \
+        python-pyparsing \
+        python-requests \
+        python-roman \
+        python-six \
+        python-sphinx \
+        python-sphinx-rtd-theme \
+        python-typing \
+        python-tz \
+        python-urllib3 \
+        python2 \
+        python2-minimal \
+        python2.7 \
+        python2.7-minimal \
+        rsync \
+        rsyslog \
+        sgml-base \
+        socat \
+        sphinx-common \
+        sphinx-rtd-theme-common \
+        ssl-cert \
+        systemd \
+        systemd-sysv \
+        unzip \
+        xml-core \
+        zip \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN adduser www-data video \
+    && a2enconf zoneminder \
+    && a2enmod rewrite
+
+# Install ZM
+COPY --from=builder /zminstall /
+
+# Configure entrypoint
+COPY entrypoint.sh /usr/local/bin/
+RUN chmod 755 /usr/local/bin/entrypoint.sh
+ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,363 @@
+#!/bin/bash
+# ZoneMinder Dockerfile entrypoint script
+# Written by Andrew Bauer <zonexpertconsulting@outlook.com>
+#
+# This script will start mysql, apache, and zoneminder services.
+# It looks in common places for the files & executables it needs
+# and thus should be compatible with major Linux distros.
+
+###############
+# SUBROUTINES #
+###############
+
+# Find ciritical files and perform sanity checks
+initialize () {
+
+    # Check to see if this script has access to all the commands it needs
+    for CMD in cat grep install ln my_print_defaults mysql mysqladmin mysqld_safe mysqlshow sed sleep su tail usermod head file; do
+      type $CMD &> /dev/null
+
+      if [ $? -ne 0 ]; then
+        echo
+        echo "ERROR: The script cannot find the required command \"${CMD}\"."
+        echo
+        exit 1
+      fi
+    done
+
+    # Look in common places for the mysqld/MariaDB executable
+    for FILE in "/usr/sbin/mysqld" "/usr/libexec/mysqld" "/usr/local/sbin/mysqld" "/usr/local/libexec/mysqld"; do
+        if [ -f $FILE ]; then
+            MYSQLD=$FILE
+            break
+        fi
+    done
+
+    # Look in common places for the apache executable commonly called httpd or apache2
+    for FILE in "/usr/sbin/httpd" "/usr/sbin/apache2"; do
+        if [ -f $FILE ]; then
+            HTTPBIN=$FILE
+            break
+        fi
+    done
+
+    # Look in common places for the zoneminder config file - zm.conf
+    for FILE in "/etc/zm.conf" "/etc/zm/zm.conf" "/usr/local/etc/zm.conf" "/usr/local/etc/zm/zm.conf"; do
+        if [ -f $FILE ]; then
+            ZMCONF=$FILE
+            break
+        fi
+    done
+
+    # Look in common places for the zoneminder startup perl script - zmpkg.pl
+    for FILE in "/usr/bin/zmpkg.pl" "/usr/local/bin/zmpkg.pl"; do
+        if [ -f $FILE ]; then
+            ZMPKG=$FILE
+            break
+        fi
+    done
+
+    # Look in common places for the zoneminder dB creation script - zm_create.sql
+    for FILE in "/usr/share/zoneminder/db/zm_create.sql" "/usr/local/share/zoneminder/db/zm_create.sql"; do
+        if [ -f $FILE ]; then
+            ZMCREATE=$FILE
+            break
+        fi
+    done
+
+    # Look in common places for the php.ini relevant to zoneminder
+    # Search order matters here because debian distros commonly have multiple php.ini's
+    for FILE in "/etc/php/7.2/apache2/php.ini" "/etc/php/7.0/apache2/php.ini" "/etc/php5/apache2/php.ini" "/etc/php.ini" "/usr/local/etc/php.ini"; do
+        if [ -f $FILE ]; then
+            PHPINI=$FILE
+            break
+        fi
+    done
+
+    for FILE in $ZMCONF $ZMPKG $ZMCREATE $PHPINI $HTTPBIN $MYSQLD; do
+        if [ -z $FILE ]; then
+            echo
+            echo "FATAL: This script was unable to determine one or more critical files. Cannot continue."
+            echo
+            echo "VARIABLE DUMP"
+            echo "-------------"
+            echo
+            echo "Path to zm.conf: ${ZMCONF}"
+            echo "Path to zmpkg.pl: ${ZMPKG}"
+            echo "Path to zm_create.sql: ${ZMCREATE}"
+            echo "Path to php.ini: ${PHPINI}"
+            echo "Path to Apache executable: ${HTTPBIN}"
+            echo "Path to Mysql executable: ${MYSQLD}"
+            echo
+            exit 98
+        fi
+    done
+
+    counter=0
+    for CREDENTIAL in $ZM_DB_HOST $ZM_DB_USER $ZM_DB_PASS $ZM_DB_NAME; do
+        if [ -n "$CREDENTIAL" ]; then
+            counter=$((counter+1))
+        fi
+    done
+
+    # counter = 0 means a local database
+    # counter = 4 means a remote database
+    # counter != 0 or 4 means the credentials were not specified correctly and we should fail
+    remoteDB=0
+    if [ "$counter" -eq "4" ]; then
+        echo " * Remote database credentials detected. Continuing..."
+        remoteDB=1
+    elif [ "$counter" -ne "0" ]; then
+        echo " * Fatal: Remote database credentials not set correctly."
+        exit 97
+    fi
+}
+
+# Usage: get_mysql_option SECTION VARNAME DEFAULT
+# result is returned in $result
+# We use my_print_defaults which prints all options from multiple files,
+# with the more specific ones later; hence take the last match.
+get_mysql_option (){
+        result=`my_print_defaults "$1" | sed -n "s/^--$2=//p" | tail -n 1`
+        if [ -z "$result" ]; then
+            # not found, use default
+            result="$3"
+        fi
+}
+
+# Return status of mysql service
+mysql_running () {
+    if [ "$remoteDB" -eq "1" ]; then
+        mysqladmin ping -u${ZM_DB_USER} -p${ZM_DB_PASS} -h${ZM_DB_HOST} > /dev/null 2>&1
+    else
+        mysqladmin ping > /dev/null 2>&1
+    fi
+    local result="$?"
+    if [ "$result" -eq "0" ]; then
+        echo "1" # mysql is running
+    else
+        echo "0" # mysql is not running
+    fi
+}
+
+# Blocks until mysql starts completely or timeout expires
+mysql_timer () {
+    timeout=60
+    count=0
+    while [ "$(mysql_running)" -eq "0" ] && [ "$count" -lt "$timeout" ]; do
+        sleep 1 # Mysql has not started up completely so wait one second then check again
+        count=$((count+1))
+    done
+
+    if [ "$count" -ge "$timeout" ]; then
+       echo " * Warning: Mysql startup timer expired!"
+    fi
+}
+
+mysql_datadir_exists() {
+    if [ -d /var/lib/mysql/mysql ]; then
+        echo "1" # datadir exists
+    else
+        echo "0" # datadir does not exist
+    fi
+}
+
+zm_db_exists() {
+    if [ "$remoteDB" -eq "1" ]; then
+        mysqlshow -u${ZM_DB_USER} -p${ZM_DB_PASS} -h${ZM_DB_HOST} ${ZM_DB_NAME} > /dev/null 2>&1
+    else
+        mysqlshow zm > /dev/null 2>&1
+    fi
+    RETVAL=$?
+    if [ "$RETVAL" = "0" ]; then
+        echo "1" # ZoneMinder database exists
+    else
+        echo "0" # ZoneMinder database does not exist
+    fi
+}
+
+# The secret sauce to determine wether to use mysql_install_db
+# or mysqld --initialize seems to be wether mysql_install_db is a shell
+# script or a binary executable
+use_mysql_install_db () {
+    local result="$?"
+
+    if [ "$result" -eq "0" ] && [ -n "$MYSQL_INSTALL_DB"  ]; then
+        local contents=$(file -b "$MYSQL_INSTALL_DB")
+        if [[ "$contents" =~ .*ASCII.text.executable.* ]]; then
+            echo "1" # mysql_install_db is a shell script
+        else
+            echo "0" # mysql_install_db is a binary
+        fi
+    else
+        echo "0" # mysql_install_db does not exist
+    fi
+}
+
+# mysql service management
+start_mysql () {
+    # determine if we are running mariadb or mysql then guess pid location
+    if [ $(mysql --version |grep -ci mariadb) -ge "1" ]; then
+        default_pidfile="/var/run/mariadb/mariadb.pid"
+    else
+        default_pidfile="/var/run/mysqld/mysqld.pid"
+    fi
+
+    # verify our guessed pid file location is right
+    get_mysql_option mysqld_safe pid-file $default_pidfile
+    mypidfile=$result
+    mypidfolder=${mypidfile%/*}
+    mysocklockfile=${mypidfolder}/mysqld.sock.lock
+
+    if [ "$(mysql_datadir_exists)" -eq "0" ]; then
+        echo " * First run of MYSQL, initializing DB."
+        MYSQL_INSTALL_DB=$(type -p mysql_install_db)
+        if [ "$(use_mysql_install_db)" -eq "1" ]; then
+            ${MYSQL_INSTALL_DB} --user=mysql --datadir=/var/lib/mysql/ > /dev/null 2>&1
+        else
+            ${MYSQLD} --initialize-insecure --user=mysql --datadir=/var/lib/mysql/ > /dev/null 2>&1
+        fi
+    elif [ -e ${mysocklockfile} ]; then
+        echo " * Removing stale lock file"
+        rm -f ${mysocklockfile}
+    fi
+    # Start mysql only if it is not already running
+    if [ "$(mysql_running)" -eq "0" ]; then
+        echo -n " * Starting MySQL database server service"
+        test -e $mypidfolder || install -m 755 -o mysql -g root -d $mypidfolder
+        mysqld_safe --user=mysql --timezone="$TZ" > /dev/null 2>&1 &
+        RETVAL=$?
+        if [ "$RETVAL" = "0" ]; then
+            echo "   ...done."
+            mysql_timer # Now wait until mysql finishes its startup
+        else
+            echo "   ...failed!"
+        fi
+    else
+        echo " * MySQL database server already running."
+    fi
+
+    mysqlpid=`cat "$mypidfile" 2>/dev/null`
+}
+
+# Check the status of the remote mysql server using supplied credentials
+chk_remote_mysql () {
+    if [ "$remoteDB" -eq "1" ]; then
+        echo -n " * Looking for remote database server"
+        if [ "$(mysql_running)" -eq "1" ]; then
+            echo "   ...found."
+        else
+            echo "   ...failed!"
+            return
+        fi
+        echo -n " * Looking for existing remote database"
+        if [ "$(zm_db_exists)" -eq "1" ]; then
+            echo "   ...found."
+        else
+            echo "   ...not found."
+            echo -n " * Attempting to create remote database using provided credentials"
+            mysql -u${ZM_DB_USER} -p${ZM_DB_PASS} -h${ZM_DB_HOST} < $ZMCREATE > /dev/null 2>&1
+            RETVAL=$?
+            if [ "$RETVAL" = "0" ]; then
+                echo "   ...done."
+            else
+                echo "   ...failed!"
+                echo " * Error: Remote database must be manually configred."
+            fi
+        fi
+    else
+        # This should never happen
+        echo " * Error: chk_remote_mysql subroutine called but no sql credentials were given!"
+    fi
+}
+
+# Apache service management
+start_http () {
+    echo -n " * Starting Apache http web server service"
+    # Debian requires we load the contents of envvars before we can start apache
+    if [ -f /etc/apache2/envvars ]; then
+        source /etc/apache2/envvars
+    fi
+    $HTTPBIN -k start > /dev/null 2>&1
+    RETVAL=$?
+    if [ "$RETVAL" = "0" ]; then
+        echo "   ...done."
+    else
+        echo "   ...failed!"
+    fi
+}
+
+# ZoneMinder service management
+start_zoneminder () {
+    echo -n " * Starting ZoneMinder video surveillance recorder"
+    $ZMPKG start > /dev/null 2>&1
+    RETVAL=$?
+    if [ "$RETVAL" = "0" ]; then
+        echo "   ...done."
+    else
+        echo "   ...failed!"
+    fi
+}
+
+cleanup () {
+    echo " * SIGTERM received. Cleaning up before exiting..."
+    kill $mysqlpid > /dev/null 2>&1
+    $HTTPBIN -k stop > /dev/null 2>&1
+    sleep 5
+    exit 0
+}
+
+################
+# MAIN PROGRAM #
+################
+
+echo
+initialize
+
+# Set the timezone before we start any services
+if [ -z "$TZ" ]; then
+    TZ="UTC"
+fi
+echo "date.timezone = $TZ" >> $PHPINI
+if [ -L /etc/localtime ]; then
+    ln -sf "/usr/share/zoneinfo/$TZ" /etc/localtime
+fi
+if [ -f /etc/timezone ]; then
+    echo "$TZ" > /etc/timezone
+fi
+
+chown -R mysql:mysql /var/lib/mysql/
+# Configure then start Mysql
+if [ "$remoteDB" -eq "1" ]; then
+    sed -i -e "s/ZM_DB_NAME=.*$/ZM_DB_NAME=$ZM_DB_NAME/g" $ZMCONF
+    sed -i -e "s/ZM_DB_USER=.*$/ZM_DB_USER=$ZM_DB_USER/g" $ZMCONF
+    sed -i -e "s/ZM_DB_PASS=.*$/ZM_DB_PASS=$ZM_DB_PASS/g" $ZMCONF
+    sed -i -e "s/ZM_DB_HOST=.*$/ZM_DB_HOST=$ZM_DB_HOST/g" $ZMCONF
+    chk_remote_mysql
+else
+    usermod -d /var/lib/mysql/ mysql > /dev/null 2>&1
+    start_mysql
+    mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'zmuser'@'localhost' IDENTIFIED BY 'zmpass';"
+    if [ "$(zm_db_exists)" -eq "0" ]; then
+        echo " * First run of mysql in the container, creating ZoneMinder dB."
+        mysql -u root < $ZMCREATE
+    else
+        echo " * ZoneMinder dB already exists, skipping table creation."
+    fi
+fi
+
+# Ensure we shutdown our services cleanly when we are told to stop
+trap cleanup SIGTERM
+
+# Start Apache
+start_http
+
+# Start ZoneMinder
+start_zoneminder
+
+# Stay in a loop to keep the container running
+while :
+do
+    # perhaps output some stuff here or check apache & mysql are still running
+    sleep 2
+done
