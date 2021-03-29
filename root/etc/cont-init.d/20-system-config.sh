d6b97997e9adb291c09eab516366e0eb63ad4f17
Alex <33379584+alexyao2015@users.noreply.github.com>
2021-03-29 00:46:29 -0400

    Apply some php optimizations

--- root/etc/cont-init.d/20-system-config.sh
+++ root/etc/cont-init.d/20-system-config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bash
 . "/usr/local/bin/logger"
-program_name="time-config"
+program_name="system-config"
 
 ## Configure Timezone
 echo "Setting system timezone to ${TZ}" | info "${program_name}"
@@ -11,8 +11,10 @@ ln -sf "/usr/share/zoneinfo/$TZ" /etc/localtime
 echo "Configuring PHP Time" | info "${program_name}"
 # PHP_INSTALL=`php -r "echo php_ini_loaded_file().PHP_EOL;"`
 PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION;" && echo -n "." && php -r "echo PHP_MINOR_VERSION;")
-echo -n ${PHP_VERSION} > /var/run/s6/container_environment/PHP_VERSION
-# Uncomment date.timezone
-sed -i "s/;date.timezone/date.timezone/" /etc/php/"${PHP_VERSION}"/fpm/php.ini
-# Configure Time
-sed -i "s:date.timezone =.*$:date.timezone = ${TZ}:" /etc/php/"${PHP_VERSION}"/fpm/php.ini
+echo -n "${PHP_VERSION}" > /var/run/s6/container_environment/PHP_VERSION
+
+echo "date.timezone = ${TZ}" >> /etc/php/"${PHP_VERSION}"/fpm/conf.d/30-zoneminder-time.ini
+
+echo "Applying PHP Optimizations" | info "${program_name}"
+
+cp -r /zoneminder/php-conf/* /etc/php/"${PHP_VERSION}"/fpm/conf.d/
