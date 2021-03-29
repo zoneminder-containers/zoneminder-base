a0d64494382e4cf4b88f6fd5249ea6f69aafab8e
Alex <33379584+alexyao2015@users.noreply.github.com>
2021-03-29 00:53:51 -0400

    More optimizations

--- root/etc/cont-init.d/20-system-config.sh
+++ root/etc/cont-init.d/20-system-config.sh
@@ -16,5 +16,8 @@ echo -n "${PHP_VERSION}" > /var/run/s6/container_environment/PHP_VERSION
 echo "date.timezone = ${TZ}" >> /etc/php/"${PHP_VERSION}"/fpm/conf.d/30-zoneminder-time.ini
 
 echo "Applying PHP Optimizations" | info "${program_name}"
-
+sed -i "s/pm.max_children =.*/pm.max_children = 120/" /etc/php/"${PHP_VERSION}"/fpm/pool.d/www.conf
+sed -i "s/pm.start_servers =.*/pm.start_servers = 12/" /etc/php/"${PHP_VERSION}"/fpm/pool.d/www.conf
+sed -i "s/pm.min_spare_servers =.*/pm.min_spare_servers = 6/" /etc/php/"${PHP_VERSION}"/fpm/pool.d/www.conf
+sed -i "s/pm.max_spare_servers =.*/pm.max_spare_servers = 18/" /etc/php/"${PHP_VERSION}"/fpm/pool.d/www.conf
 cp -r /zoneminder/php-conf/* /etc/php/"${PHP_VERSION}"/fpm/conf.d/
