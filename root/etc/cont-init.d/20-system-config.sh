efb72f615e1e3555c4d80ddf9b4aec238f227ccc
Alex <33379584+alexyao2015@users.noreply.github.com>
2021-03-30 00:34:22 -0400

    Replace Apache2 with Nginx (#9)
    * Install nginx
    
    * Make webui work
    
    Fix fcgiwrap
    
    * Operational fcgiwrap
    
    * Apply some php optimizations
    
    * More optimizations
    
    * Remove unnecessary vars
    
    * Enable logging for everything
    
    * Remove /data from permissions fix
    
    * Reconfigure logging + first run handler
    
    * Reconfigure logging to use log folder
    
    * Fix logging + revert first run changes
    
    * Ensure services are started

---
+++ root/etc/cont-init.d/20-system-config.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/with-contenv bash
+. "/usr/local/bin/logger"
+program_name="system-config"
+
+## Configure Timezone
+echo "Setting system timezone to ${TZ}" | info "[${program_name}] "
+ln -sf "/usr/share/zoneinfo/$TZ" /etc/localtime
+
+## Set PHP Time
+
+echo "Configuring PHP Time" | info "[${program_name}] "
+# PHP_INSTALL=`php -r "echo php_ini_loaded_file().PHP_EOL;"`
+PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION;" && echo -n "." && php -r "echo PHP_MINOR_VERSION;")
+echo -n "${PHP_VERSION}" > /var/run/s6/container_environment/PHP_VERSION
+
+echo "date.timezone = ${TZ}" >> /etc/php/"${PHP_VERSION}"/fpm/conf.d/30-zoneminder-time.ini
+
+echo "Applying PHP Optimizations" | info "[${program_name}] "
+sed -i "s/pm.max_children =.*/pm.max_children = 120/" /etc/php/"${PHP_VERSION}"/fpm/pool.d/www.conf
+sed -i "s/pm.start_servers =.*/pm.start_servers = 12/" /etc/php/"${PHP_VERSION}"/fpm/pool.d/www.conf
+sed -i "s/pm.min_spare_servers =.*/pm.min_spare_servers = 6/" /etc/php/"${PHP_VERSION}"/fpm/pool.d/www.conf
+sed -i "s/pm.max_spare_servers =.*/pm.max_spare_servers = 18/" /etc/php/"${PHP_VERSION}"/fpm/pool.d/www.conf
+
+echo "memory_limit = 2048M
+max_execution_time = 600
+max_input_vars = 3000
+max_input_time = 600" > /etc/php/"${PHP_VERSION}"/fpm/conf.d/30-zoneminder.ini
--- root/etc/cont-init.d/30-zm-config.sh
+++ root/etc/cont-init.d/30-zm-config.sh
@@ -2,24 +2,27 @@
 . "/usr/local/bin/logger"
 program_name="zm-config"
 
-## Link ZoneMinder config and data folders
-
+# Configure zm config folder
 if [ ! -f "/config/zm.conf" ]; then
-  echo "Configuring ZoneMinder Configuration folder" | init "${program_name}"
+  echo "Configuring ZoneMinder Configuration folder" | init "[${program_name}] "
   s6-setuidgid www-data \
-    cp -r /zoneminder/defaultconfig/* /config
+  cp -r /zoneminder/defaultconfig/* /config
 fi
 
+# Setup required data folders
 if [ ! -d "/data/events" ]; then
-  echo "Configuring ZoneMinder Data folder" | init "${program_name}"
+  echo "Configuring ZoneMinder Data folder" | init "[${program_name}] "
+  # Only configure data permissions once to prevent massive startup lag
+  chmod -R 755 /data
+  chown www-data:www-data /data
   s6-setuidgid www-data \
     mkdir -p \
-      /data/events \
-      /data/images
+    /data/events \
+    /data/images
 fi
 
 ## Configure ZoneMinder DB
-echo "Configuring ZoneMinder db Settings" | info "${program_name}"
+echo "Configuring ZoneMinder db Settings" | info "[${program_name}] "
 sed -i "s/ZM_DB_USER=.*$/ZM_DB_USER=${MYSQL_USER}/g" /config/zm.conf
 sed -i "s/ZM_DB_PASS=.*$/ZM_DB_PASS=${MYSQL_PASSWORD}/g" /config/zm.conf
 sed -i "s/ZM_DB_HOST=.*$/ZM_DB_HOST=${MYSQL_HOST}/g" /config/zm.conf
@@ -27,6 +30,6 @@ sed -i "s/ZM_DB_HOST=.*$/ZM_DB_HOST=${MYSQL_HOST}/g" /config/zm.conf
 sed -i "s/ZM_DB_NAME=.*$/ZM_DB_NAME=zm/g" /config/zm.conf
 
 if [[ -n "${ZM_SERVER_HOST}" ]]; then
-  echo "Configuring ZoneMinder ZM_SERVER_HOST for Multi-Server Support" | info "${program_name}"
+  echo "Configuring ZoneMinder ZM_SERVER_HOST for Multi-Server Support" | info "[${program_name}] "
   sed -i "s/ZM_SERVER_HOST=.*$/ZM_SERVER_HOST=${ZM_SERVER_HOST}/g" /config/zm.conf
 fi
