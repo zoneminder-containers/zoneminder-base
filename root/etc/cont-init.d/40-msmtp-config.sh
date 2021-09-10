56f69d236b8feef59fea30aad2caca43200a5c75
khimaros <me@khimaros.com>
2021-09-10 06:25:13 -0700

    allow specifying smtp user independent of address (#16)

--- root/etc/cont-init.d/40-msmtp-config.sh
+++ root/etc/cont-init.d/40-msmtp-config.sh
@@ -2,8 +2,11 @@
 . "/usr/local/bin/logger"
 program_name="msmtp-config"
 
+EMAIL_USER=${EMAIL_USER:-"${EMAIL_ADDRESS}"}
+
 echo "Configuring msmtp settings..." | info "[${program_name}] "
 sed -i "s/EMAIL_HOST/${EMAIL_HOST}/g" /etc/msmtprc
 sed -i "s/EMAIL_PORT/${EMAIL_PORT}/g" /etc/msmtprc
 sed -i "s/EMAIL_ADDRESS/${EMAIL_ADDRESS}/g" /etc/msmtprc
+sed -i "s/EMAIL_USER/${EMAIL_USER}/g" /etc/msmtprc
 sed -i "s/EMAIL_PASSWORD/${EMAIL_PASSWORD}/g" /etc/msmtprc
